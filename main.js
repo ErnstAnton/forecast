e2abf788581992e2d479b367b36e324f323312d7
ErnstAnton <ernst.schindler@student.uibk.ac.at>
2024-05-16 17:26:50 +0200

    winddaten asynchron laden

--- main.js
+++ main.js
@@ -81,6 +81,15 @@ map.on("click", function(evt) {
     showForecast(`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`);
 });
 
+//Klick auf Innsbruck simulieren
 map.fire("click", {
     latlng: ibk
 });
+
+//Windkarte
+async function loadWind(url) {
+    const response = await fetch(url);
+    const jsondata = await response.json();
+    console.log(jsondata);
+}
+loadWind("https://geographie.uibk.ac.at/data/ecmwf/data/wind-10u-10v-europe.json");
